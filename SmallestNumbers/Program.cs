f798fc9bb36cc4a5a4fffd849e31249306ae5ce6
Debashis <jupiter_104@ymail.com>
2019-03-31 11:01:08 +0530

    Add files via upload

---
+++ SmallestNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmallestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 5 or more numbers e.g. 5, 1, 9, 2, 10");
+
+                string input = Console.ReadLine();
+
+                numbers = input.Split(',').Select(int.Parse).ToList();
+
+                if (numbers.Count < 5)
+                {
+                    Console.WriteLine("Invalid list");
+                    continue;
+                }
+                else
+                {
+                    numbers.Sort();
+                    Console.WriteLine("Three smallest numbers: {0}, {1}, {2}", numbers[0], numbers[1], numbers[2]);
+                    break;
+                }                
+            }
+            Console.ReadLine();
+        }
+    }
+}
